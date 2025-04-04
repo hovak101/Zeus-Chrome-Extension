042d1fde0f0bb643d1254293baf30e498c7c4264
Alex Hovakimyan <hovak101@gmail.com>
2025-04-03 21:58:48 -0700

    create react frontend template

---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'manifest.json', dest: '.' },
+        { src: 'src/background.js', dest: '.' },
+        { src: 'src/content.js', dest: '.' },
+      ],
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+})
