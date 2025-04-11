ce968a8acb07d395e54fea28d8a850c51e067b69
Alex Hovakimyan <hovak101@gmail.com>
2025-04-10 20:53:29 -0700

    mess around with config files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,19 +6,6 @@ module.exports = {
       "./src/**/*.{js,ts,jsx,tsx}", 
     ],
     theme: {
-      extend: {
-        colors: {
-          muted: '#f5f5f5',
-          'muted-foreground': '#6b7280',
-          background: "#ffffff",
-          input: "#f1f1f1",
-          primary: "#facc15", // yellow-400
-          ring: "#a855f7", // purple-500
-        },
-        fontFamily: {
-          lora: ['Lora', 'serif'], 
-        },
-      },
     },
     plugins: [],
 }
